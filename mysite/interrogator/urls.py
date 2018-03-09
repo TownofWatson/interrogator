e28244b38826bdfbc7e7532d44d3badcf92f3aa4
Amy Miao <miao.106@osu.edu>
2018-03-08 19:36:15 -0500

    conversation and discovery work on the website

--- mysite/interrogator/urls.py
+++ mysite/interrogator/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('simulation/', views.simulation, name='simulation'),
     path('simulation/converse/', views.converse, name='converse'),
-    #path('simulation/watson/', views.watson, name='watson')
+    path('simulation/watson/', views.watson, name='watson')
 ]
--- mysite/interrogator/views.py
+++ mysite/interrogator/views.py
@@ -4,7 +4,7 @@ from django.core.management import call_command
 from django.views.decorators.csrf import csrf_exempt
 from watson_developer_cloud import ConversationV1
 import json
-#import watson
+from watson import *
 
 conversation = ConversationV1(
 	username = 'a2f2135d-5741-4364-803b-66b8116a9b5f',
@@ -15,8 +15,8 @@ workspace_id = '225a20b1-c3c2-429b-87ba-b389eefc8853'
 
 context = {}
 
-#watson_state = 0
-#extrac = ""
+watson_state = 0
+extrac = ""
 
 # Create your views here.
 def index(request):
@@ -27,8 +27,8 @@ def simulation(request):
 	global context
 	response = conversation.message(
 	    workspace_id=workspace_id,
-	    input={'text': user_input},
-	    context = context
+	    message_input={'text': user_input},
+	    context = context,
 	)
 
 	context = response['context']
@@ -39,24 +39,23 @@ def converse(request):
 	global context
 	response = conversation.message(
 	    workspace_id=workspace_id,
-	    input={'text': request.GET.get('user_input')},
-	    context = context
+	    message_input={'text': request.GET.get('user_input')},
+	    context = context,
 	)
 
 	context = response['context']
 	
 	return HttpResponse(json.dumps(response['output']['text'][0],indent=2))
-'''
+
 def watson(request):
 	global watson_state
 	global extrac
 	if(watson_state ==0):
-		response, extrac = watson.ask_watson(request.GET.get('user_input'))
+		response, extrac = ask_watson(request.GET.get('user_input'))
 		watson_state = 1
 	elif(watson_state ==1):
-		success, response = watson.ask_watson_response(user_input, extrac)
+		success, response = ask_watson_response(request.GET.get('user_input'), extrac)
 		if(success != 0):
 			watson_state = 0
 
 	return HttpResponse(response)
-'''
